65a1be9acb8a8f30c173a2e1ba41516ab91c3656
Oblarg <emichaelbrnett@gmail.com>
2025-10-29 11:09:53 -0400

    cleanup!

--- test_mm2.rs
+++
@@ -1,1 +0,0 @@
-fn test_mm2() { let _ = whippyunits::unit!(mm^2, f64); }
--- test_parse_mm.rs
+++
@@ -1,4 +0,0 @@
-fn test_parse_mm() { 
-    let result = whippyunits_core::SiPrefix::strip_any_prefix_symbol("mm"); 
-    println!("{:?}", result); 
-}
